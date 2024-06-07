aa1f1adb0b03f48bf7a67960b26f9cabbd0cd1b5
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-06-07 14:29:02 +0300

    Edit app, add server, connected mongodb, add packages

---
+++ server/src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+// import dotenv from 'dotenv';
+// dotenv.config();
+import 'dotenv/config';
+
+import advertRoutes from '../routes/advertRoutes.js';
+
+const app = express();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/adverts", advertRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
+app.use((err, req, res, next) => {
+    if (typeof err.status !== "undefined" && typeof err.message !== "undefined") {
+        res.status(err.status).json({ message: err.message });
+    } else {
+        console.error(err.stack);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+})
+
+export default app;
