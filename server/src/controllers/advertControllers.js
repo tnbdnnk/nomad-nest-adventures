4c71781c47f99528758e2e7f2d6abf6e6060fdcf
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-06-07 17:01:14 +0300

    Add advert models, routes and controllers

---
+++ server/src/controllers/advertControllers.js
@@ -0,0 +1,21 @@
+import Advert from "../models/Advert";
+
+export const getAdverts = async (req, res) => {
+    try {
+        const adverts = await Advert.find();
+        res.status(200).json(adverts);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const getAdvertById = async (req, res) => {
+    try {
+        const advert = await Advert.findById(req.params.id);
+        if (!advert) return res.status(404).json({ message: 'Advert not found' });
+        res.status(200).json(advert);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
